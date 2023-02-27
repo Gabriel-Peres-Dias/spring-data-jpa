2a183fce070b62004994ebe2f3b46168e49171c7
Gabriel Peres <gabriel.2000mourad@gmail.com>
2023-02-27 20:53:25 -0300

    Criando primeiro insert

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,14 +1,34 @@
 package br.com.alura.spring.data;
 
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //anotação que faz o com que o spring percorra nas anotações do projeto
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+	private final CargoRepository repository;
+
+	public SpringDataApplication (CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		criarCargo("DESENVOLVEDOR DE SOFTWARE");
+	}
+
+	public void criarCargo(String cargo) {
+		Cargo cargoModel = new Cargo();
+		cargoModel.setDescricao(cargo);
+		repository.save(cargoModel);
+	}
 }
