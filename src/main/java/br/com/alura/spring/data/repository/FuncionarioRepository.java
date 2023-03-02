9992ef9da3442f5c799a0ad64161a77e55c8dfc8
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-03-02 20:31:37 -0300

    removendo comentarios

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -15,20 +15,15 @@ import java.util.List;
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>, PagingAndSortingRepository<Funcionario, Integer>, JpaSpecificationExecutor<Funcionario> {
 
-    //Derived query
     List<Funcionario> findByNome(String nome);
 
-
-    //jpql
     @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome " +
             "AND f.salario >= :salario AND f.dataContratacao = :dataContratacao")
     List<Funcionario> buscarNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate dataContratacao);
 
-    //native query, buscas nativas do banco que utilizamos
     @Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
     List<Funcionario> findDataContratacaoMaior(LocalDate data);
 
-    //projeção, entidades que contem apenas os atributos que queremos
     @Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
     List<FuncionarioProjecao> findFuncionarioSalario();
 
