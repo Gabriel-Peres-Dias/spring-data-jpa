250694dc1e32b16f9dcf4cd7608104c1885a88a8
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-03-02 20:29:26 -0300

    Finalizando a implementação do serviço de relatorio dinamico + último commit do curso :(

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -2,76 +2,67 @@ package br.com.alura.spring.data;
 
 import java.util.Scanner;
 
-import br.com.alura.spring.data.service.RelatoriosService;
+import br.com.alura.spring.data.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import br.com.alura.spring.data.service.CrudCargoService;
-import br.com.alura.spring.data.service.CrudFuncionarioService;
-import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
-
 @EnableJpaRepositories
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-	private Boolean system = true;
+    private Boolean system = true;
 
-	private final CrudCargoService cargoService;
+    private final CrudCargoService cargoService;
+    private final CrudFuncionarioService funcionarioService;
+    private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+    private final RelatoriosService relatoriosService;
+    private final RelatorioFuncionarioDinamicoService relatorioFuncionarioDinamicoService;
 
-	private final CrudFuncionarioService funcionarioService;
+    public SpringDataApplication(
+            CrudCargoService cargoService,
+            CrudFuncionarioService funcionarioService,
+            CrudUnidadeTrabalhoService unidadeTrabalhoService,
+            RelatoriosService relatoriosService,
+            RelatorioFuncionarioDinamicoService relatorioFuncionarioDinamicoService) {
+        this.cargoService = cargoService;
+        this.funcionarioService = funcionarioService;
+        this.unidadeTrabalhoService = unidadeTrabalhoService;
+        this.relatoriosService = relatoriosService;
+        this.relatorioFuncionarioDinamicoService = relatorioFuncionarioDinamicoService;
+    }
 
-	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataApplication.class, args);
+    }
 
-	private final RelatoriosService relatoriosService;
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
 
-	public SpringDataApplication(CrudCargoService cargoService,
-			CrudFuncionarioService funcionarioService, 
-			CrudUnidadeTrabalhoService unidadeTrabalhoService,
-			RelatoriosService relatoriosService) {
-		this.cargoService = cargoService;
-		this.funcionarioService = funcionarioService;
-		this.unidadeTrabalhoService = unidadeTrabalhoService;
-		this.relatoriosService = relatoriosService;
-	}
+        while (system) {
+            System.out.println("Qual função deseja executar?");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Cargo");
+            System.out.println("2 - Funcionario");
+            System.out.println("3 - Unidade");
+            System.out.println("4 - Relatorios");
+            System.out.println("5 - Relatorio Dinâmico");
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataApplication.class, args);
-	}
+            Integer function = scanner.nextInt();
 
-	@Override
-	public void run(String... args) throws Exception {
-		Scanner scanner = new Scanner(System.in);
-
-		while (system) {
-			System.out.println("Qual função deseja executar?");
-			System.out.println("0 - Sair");
-			System.out.println("1 - Cargo");
-			System.out.println("2 - Funcionario");
-			System.out.println("3 - Unidade");
-			System.out.println("4 - Relatorios");
-			
-			Integer function = scanner.nextInt();
-
-			switch (function) {
-				case 1:
-					cargoService.inicial(scanner);
-					break;
-				case 2:
-					funcionarioService.inicial(scanner);
-					break;
-				case 3:
-					unidadeTrabalhoService.inicial(scanner);
-					break;
-				case 4:
-					relatoriosService.inicial(scanner);
-					break;
-				default:
-					System.out.println("Finalizando");
-					system = false;
-					break;
-			}
-		}
-	}
+            switch (function) {
+                case 1 -> cargoService.inicial(scanner);
+                case 2 -> funcionarioService.inicial(scanner);
+                case 3 -> unidadeTrabalhoService.inicial(scanner);
+                case 4 -> relatoriosService.inicial(scanner);
+                case 5 -> relatorioFuncionarioDinamicoService.inicial(scanner);
+                default -> {
+                    System.out.println("Finalizando");
+                    system = false;
+                }
+            }
+        }
+    }
 }
